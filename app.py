74001cc4ebffb152b41bb4aecc5a603191b12aa7
Adilson José dos Santos <adilson.santos@AGU.GOV.BR>
2020-12-25 23:59:12 -0300

    Inclui o requirements do projeto, o app_restful com exemplo de aplicacao com a extencao flask_restful e incluido o modulo habilidades para gerencias as habilidades dos desenvolvedores.

--- app.py
+++ app.py
@@ -1,48 +1,48 @@
-from flask import Flask, jsonify,request  # Depois de instalado importar o Flask e o jsonify
-import json
+# from flask import Flask, jsonify,request  # Depois de instalado importar o Flask e o jsonify
+# import json
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-desenvolvedores = [
+# desenvolvedores = [
+#
+#     {'nome':'Rafael',
+#      'habilidades':['Python','Flask']
+#      },
+#     {'nome': 'Adilson',
+#      'habilidades':['PHP','Symfony']
+#      }
+# ]
+#
+# @app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE']) # Adicionando a rota atravez do decorador
+# def desenvolvedor(id):
+#     if request.method == 'GET':
+#         try:
+#             response = desenvolvedores[id]
+#         except IndexError:
+#             mensagem = 'Desenvolvedor de Id {} não existe' .format(id)
+#             response = {'status':'erro', 'mensagem':mensagem}
+#         except Exception:
+#             mensagem = 'Erro desconhecido. Procure o desenvolvedor da API'
+#             response = {'status': 'erro', 'mensagem': mensagem}
+#         return jsonify(response)
+#     elif request.method == 'PUT':
+#         dados = json.loads(request.data)
+#         desenvolvedores[id] = dados
+#         return jsonify(dados)
+#     elif request.method == 'DELETE':
+#         desenvolvedores.pop(id)
+#         return jsonify({'status':'sucesso', 'mensagem':'Registro excluído com sucesso!'})
+#
+# @app.route('/dev/', methods=['POST', 'GET'])
+# def lista_desenvolvedores():
+#     if request.method == 'POST':
+#         dados = json.loads(request.data)
+#         desenvolvedores.append(dados)
+#         return jsonify({'status':'sucesso', 'mensagem':'Registro inserido com sucesso!'})
+#     elif request.method == 'GET':
+#         return jsonify(desenvolvedores)
 
-    {'nome':'Rafael',
-     'habilidades':['Python','Flask']
-     },
-    {'nome': 'Adilson',
-     'habilidades':['PHP','Symfony']
-     }
-]
 
-@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE']) # Adicionando a rota atravez do decorador
-def desenvolvedor(id):
-    if request.method == 'GET':
-        try:
-            response = desenvolvedores[id]
-        except IndexError:
-            mensagem = 'Desenvolvedor de Id {} não existe' .format(id)
-            response = {'status':'erro', 'mensagem':mensagem}
-        except Exception:
-            mensagem = 'Erro desconhecido. Procure o desenvolvedor da API'
-            response = {'status': 'erro', 'mensagem': mensagem}
-        return jsonify(response)
-    elif request.method == 'PUT':
-        dados = json.loads(request.data)
-        desenvolvedores[id] = dados
-        return jsonify(dados)
-    elif request.method == 'DELETE':
-        desenvolvedores.pop(id)
-        return jsonify({'status':'sucesso', 'mensagem':'Registro excluído com sucesso!'})
-
-@app.route('/dev/', methods=['POST', 'GET'])
-def lista_desenvolvedores():
-    if request.method == 'POST':
-        dados = json.loads(request.data)
-        desenvolvedores.append(dados)
-        return jsonify({'status':'sucesso', 'mensagem':'Registro inserido com sucesso!'})
-    elif request.method == 'GET':
-        return jsonify(desenvolvedores)
-
-
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#    app.run()
 
